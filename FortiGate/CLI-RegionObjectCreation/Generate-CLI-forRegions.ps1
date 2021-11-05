c516457ba9963606f38cd4b3a88f71e3d6aaaea0
mRiston <mikeriston+GitHub@gmail.com>
2021-11-05 09:07:56 -0400

    Add FortiGate\CLI-RegionObjectCreation

---
+++ FortiGate/CLI-RegionObjectCreation/Generate-CLI-forRegions.ps1
@@ -0,0 +1,24 @@
+﻿#script generation to create objects in FortiGate to allow for region-based policies.
+
+$Raw = Import-Csv C:\scripts\FortiNetCountryCodes.csv -Delimiter '|'
+
+<#$TemplateCode = "edit `"$($line.CountryName.Replace(',',''))`"
+set type geography
+set country $($line.CountryCode)
+set color 24
+next
+"
+#>
+$TemplateCode = "edit `"{0}`"
+set type geography
+set country {1}
+set color 24
+next
+"
+$ToRun = @()
+$ToRun += "config firewall address"
+foreach ($line in $Raw){
+  $code = $line.CountryCode
+  $name = $line.CountryName.Replace(',','')
+  $ToRun +=   $TemplateCode -f $name,$code
+}
